8282e61d8add6ffa5d2cb171ab5ddceb1cc77b2c
papayaa006 <11160791@me.mcu.edu.tw>
2023-02-17 12:08:23 +0800

    修改目錄

---
+++ week01/week01-2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    printf("請輸入字母: \n");
+    char c;
+    scanf("%c", &c);
+    printf("你輸入 %c\n", c);
+
+    printf("請輸入整數: \n");
+    int a;
+    scanf("%d", &a);
+    printf("你輸入%d\n", a);
+
+}
+
